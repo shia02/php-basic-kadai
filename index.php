350f733ebccd887a7fd7f8433d1a58e5da8f1d26
shia <nahsor2242195@gmail.com>
2025-02-20 11:09:42 +0900

    kadai_005

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>この文章はHTMLで出力しています。</p>
+   <p>
+       <?php
+       $username;
+       $username = 'taro';
+       echo $username;
+       ?>
+   </p>
+</body>
+
+</html>
