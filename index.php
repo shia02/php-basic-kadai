0e671d41114acb3b4146e2e112cb7882f6432105
shia <nahsor2242195@gmail.com>
2025-02-21 19:54:10 +0900

    kadai_011

--- index.php
+++ index.php
@@ -11,15 +11,9 @@
    <p>
        <?php
        $user_names = ['侍太郎', '侍一郎', '侍二郎', '侍三郎', '侍四郎'];
-       print_r($user_names);
-       echo '<br>';
-       echo $user_names[2];
-       $personal_data1 = ['name' => '侍太郎', 'age' => 36, 'gender' => '男性', 'address' => '東京都', 'phone_number' => '070-0809-1160'];
-       echo '<br>';
-       $personal_data1['test'] = 'test';
-       echo $personal_data1['name'];
-       echo '<br>';
-       print_r($personal_data1);
+       foreach ($user_names as $index => $value) {
+         echo "{$index}：{$value}<br>";
+       }
        ?>
    </p>
 </body>
