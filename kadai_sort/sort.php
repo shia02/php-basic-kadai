5141255648b61f348b5e1f605cad5f261cd76003
shia <nahsor2242195@gmail.com>
2025-02-22 18:00:02 +0900

    kadai_sort

---
+++ kadai_sort/sort.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $nums = [15, 4, 18, 23, 10 ];
+        function sort_2way($array, bool $order) {
+            if ($order) {
+                echo '昇順にソートします。<br>';
+                sort($array);
+            } else {
+                echo '降順にソートします。<br>';
+                rsort($array);
+            }
+            foreach($array as $element) {
+                echo $element . '<br>';
+            }
+        }
+        sort_2way($nums, true);
+        sort_2way($nums, false);
+        ?>
+    </p>
+</body>
+
+</html>
